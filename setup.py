6df9f1cc6f82921de6af491ce2c59ffa4ac38fc8
Michael Lamertz <michael.lamertz@gmail.com>
2023-02-14 22:54:32 +0100

    Working on packaging
    ...still figuring shit out

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="tinysvg",
+    version="0.1.0",
+    author="Michael Lamertz",
+    author_email="michael.lamertz@gmail.com",
+    url="https://github.com/dickerdackel/tinysvg",
+    packages=[],
+)
